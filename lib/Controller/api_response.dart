4501fa78be4662abd71b9d74d471c28c696a657e
Faisal Aslam <faisalaslam218@gmail.com>
2024-06-25 22:01:06 +0500

    Improvements

--- lib/Controller/api_response.dart
+++ lib/Controller/api_response.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:quiz_app/Model/quiz_model.dart';
 
 class ApiClass {
-  String url = 'https://opentdb.com/api.php?amount=20&category=25';
+  String url = 'https://opentdb.com/api.php?amount=20&category=9';
 
   Future<QuizModel> fetchData() async {
     final response = await http.get(Uri.parse(url));
